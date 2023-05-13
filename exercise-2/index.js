414937b07497f87851dd0e6f95a3b8b6c8d2a5e7
L3Bao <s3979654@rmit.edu.vn>
2023-05-13 17:06:06 +0700

    b

--- exercise-2/index.js
+++ exercise-2/index.js
@@ -79,6 +79,34 @@ connectDB();
 app.get('/signup', (req, res) => {
     res.render('signup', { user: req.user }); // Pass the user variable to the signup template
 });
+app.post('/signup', async (req, res) => {
+    const { username, password, role } = req.body;
+    
+    try {
+      const existingUser = await User.findOne({ username });
+      
+      if (existingUser) {
+        const errorMessage = 'Username already exists';
+        return res.status(409).render('signup', { errorMessage, user: req.user });
+      }
+      
+      const user = new User({
+        username,
+        password,
+        role
+      });
+      
+      await user.save();
+      res.redirect('/login');
+    } catch (error) {
+      console.error('Error creating user:', error);
+      res.status(500).send('An error occurred while creating the user');
+    }
+  });
+  
+  
+  
+  
 
 
 app.get('/', (req, res) => {
@@ -104,11 +132,35 @@ app.get('/dashboard', (req, res) => {
 });
 
 
-app.post('/login', passport.authenticate('local', {
-    successRedirect: '/dashboard',
-    failureRedirect: '/login',
-    failureFlash: true,
-}));
+app.post('/login', (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            // Authentication failed, redirect to login page with error message and previous input
+            return res.render('login', {
+                user: req.user,
+                message: 'Invalid username or password.',
+                username: req.body.username, // Keep the username input
+                password: req.body.password // Keep the password input
+            });
+        }
+        req.logIn(user, (err) => {
+            if (err) {
+                return next(err);
+            }
+            // Authentication successful, redirect to dashboard or appropriate page
+            if (req.user.role === 'vendor') {
+                return res.redirect('/vendor-dashboard');
+            } else if (req.user.role === 'customer') {
+                return res.redirect('/customer-dashboard');
+            } else {
+                return res.status(403).send('Access denied');
+            }
+        });
+    })(req, res, next);
+});
 
 
 // Middleware to check if user is authenticated and has the required role
@@ -134,16 +186,16 @@ const authenticate = (req, res, next) => {
 // Route to manage order page
 app.get('/manage-orders', authenticate, async (req, res) => {
     try {
-      const orders = await Order.find().populate('products');
-  
-      res.render('manage-orders', { orders, user: req.user });
+        const orders = await Order.find().populate('products');
+
+        res.render('manage-orders', { orders, user: req.user });
     } catch (error) {
-      console.error('Error retrieving orders:', error);
-      res.status(500).send('An error occurred while retrieving orders');
+        console.error('Error retrieving orders:', error);
+        res.status(500).send('An error occurred while retrieving orders');
     }
-  });
-  
-  
+});
+
+
 
 app.get('/vendor-dashboard', async (req, res) => {
     try {
@@ -172,9 +224,9 @@ app.get('/customer-dashboard', async (req, res) => {
 // Route protected by authentication middleware and restricted to vendors
 app.get('/add-product', authenticate, (req, res) => {
     res.render('add-product', { user: req.user }); // Pass the user variable to the add-product template
-    });
-    
-    app.post('/add-product', authenticate, upload.single('image'), async (req, res) => {
+});
+
+app.post('/add-product', authenticate, upload.single('image'), async (req, res) => {
     const { name, price, description } = req.body;
     const image = req.file ? req.file.path : '';
     try {
@@ -184,78 +236,79 @@ app.get('/add-product', authenticate, (req, res) => {
             description,
             image
         });
-    
+
         await product.save();
         res.redirect('/vendor-dashboard');
     } catch (error) {
         console.error('Error adding new product:', error);
         res.status(500).send('An error occurred while adding the product');
-    }});
+    }
+});
 
-    app.get('/edit-product/:id', authenticate, async (req, res) => {
-        try {
-            const productId = req.params.id;
-    
-            if (!mongoose.Types.ObjectId.isValid(productId)) {
-                return res.status(400).send('Invalid product ID');
-            }
-    
-            const product = await Product.findOne({ _id: productId });
-    
-            if (!product) {
-                return res.status(404).send('Product not found');
-            }
-    
-            res.render('edit-product', { product, user: req.user });
-        } catch (error) {
-            console.error('Error retrieving product:', error);
-            res.status(500).send('An error occurred while retrieving the product');
+app.get('/edit-product/:id', authenticate, async (req, res) => {
+    try {
+        const productId = req.params.id;
+
+        if (!mongoose.Types.ObjectId.isValid(productId)) {
+            return res.status(400).send('Invalid product ID');
         }
-    });
-    
-    
-      
-      
 
-    app.post('/update-product/:id', async (req, res) => {
-        try {
-          const product = await Product.findById(req.params.id);
-      
-          if (!product) {
+        const product = await Product.findOne({ _id: productId });
+
+        if (!product) {
+            return res.status(404).send('Product not found');
+        }
+
+        res.render('edit-product', { product, user: req.user });
+    } catch (error) {
+        console.error('Error retrieving product:', error);
+        res.status(500).send('An error occurred while retrieving the product');
+    }
+});
+
+
+
+
+
+app.post('/update-product/:id', async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+
+        if (!product) {
             return res.status(404).json({ message: 'Product not found' });
-          }
-      
-          // Update the fields that have been changed
-          if (req.body.name) {
+        }
+
+        // Update the fields that have been changed
+        if (req.body.name) {
             product.name = req.body.name;
-          }
-          if (req.body.price) {
+        }
+        if (req.body.price) {
             product.price = req.body.price;
-          }
-          if (req.body.description) {
+        }
+        if (req.body.description) {
             product.description = req.body.description;
-          }
-      
-          // Check if the user uploaded a new image
-          if (req.files && req.files.image) {
+        }
+
+        // Check if the user uploaded a new image
+        if (req.files && req.files.image) {
             // Handle image upload here
             // ...
-      
+
             // Update the image field
             product.image = updatedImageUrl;
-          }
-      
-          await product.save();
-      
-          res.redirect(`/product/${product._id}`);
-        } catch (err) {
-          console.error(err);
-          res.status(500).send('Error updating product');
         }
-      });
 
-      
-    
+        await product.save();
+
+        res.redirect(`/product/${product._id}`);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error updating product');
+    }
+});
+
+
+
 
 app.post('/delete-product/:id', authenticate, async (req, res) => {
     try {
@@ -323,88 +376,90 @@ app.post('/profile', async (req, res) => {
     }
 });
 
-//logout
+// Logout route
 app.get('/logout', (req, res) => {
-    // Clear the user session or token
-    // Assuming you are using sessions, you would do:
-    req.session.destroy();
-
-    // Redirect the user to the login page
-    res.redirect('/login');
+    req.session.destroy((err) => {
+        if (err) {
+            console.error('Error destroying session:', err);
+        }
+        res.redirect('/login');
+    });
 });
+
+
 
 //order page
 app.get('/order-page', async (req, res) => {
     try {
-      const products = await Product.find(); // Retrieve products from the MongoDB collection
-  
-      res.render('order', { products, user: req.user });
-    } catch (error) {
-      console.error('Error retrieving products:', error);
-      res.status(500).send('An error occurred while retrieving products');
-    }
-  });
-  app.post('/update-order/:id', authenticate, async (req, res) => {
-    try {
-      const orderId = req.params.id;
-      // Retrieve updated order details from the request body
-      const { customerName, products } = req.body;
-  
-      // Update the order in the database
-      const updatedOrder = await Order.findByIdAndUpdate(
-        orderId,
-        { customerName, products },
-        { new: true }
-      );
-  
-      res.redirect('/manage-orders');
-    } catch (error) {
-      console.error('Error updating order:', error);
-      res.status(500).send('An error occurred while updating the order');
-    }
-  });
+        const products = await Product.find(); // Retrieve products from the MongoDB collection
 
-  app.get('/edit-order/:id', authenticate, async (req, res) => {
-    try {
-      const orderId = req.params.id;
-      const order = await Order.findById(orderId).populate('products');
-      const products = await Product.find();
-  
-      res.render('edit-order', { order, products, user: req.user });
+        res.render('order', { products, user: req.user });
     } catch (error) {
-      console.error('Error retrieving order:', error);
-      res.status(500).send('An error occurred while retrieving the order');
+        console.error('Error retrieving products:', error);
+        res.status(500).send('An error occurred while retrieving products');
     }
-  });
-  
-  app.post('/update-order/:id', authenticate, async (req, res) => {
+});
+app.post('/update-order/:id', authenticate, async (req, res) => {
     try {
-      const orderId = req.params.id;
-      const { customerName, products } = req.body;
-  
-      // Find the order in the database
-      const order = await Order.findById(orderId);
-  
-      if (!order) {
-        return res.status(404).send('Order not found');
-      }
-  
-      // Update the order data
-      order.customerName = customerName;
-      order.products = products;
-  
-      // Save the updated order
-      await order.save();
-  
-      res.redirect('/manage-orders');
+        const orderId = req.params.id;
+        // Retrieve updated order details from the request body
+        const { customerName, products } = req.body;
+
+        // Update the order in the database
+        const updatedOrder = await Order.findByIdAndUpdate(
+            orderId,
+            { customerName, products },
+            { new: true }
+        );
+
+        res.redirect('/manage-orders');
     } catch (error) {
-      console.error('Error updating order:', error);
-      res.status(500).send('An error occurred while updating the order');
+        console.error('Error updating order:', error);
+        res.status(500).send('An error occurred while updating the order');
     }
-  });
-  
-  
-   
+});
+
+app.get('/edit-order/:id', authenticate, async (req, res) => {
+    try {
+        const orderId = req.params.id;
+        const order = await Order.findById(orderId).populate('products');
+        const products = await Product.find();
+
+        res.render('edit-order', { order, products, user: req.user });
+    } catch (error) {
+        console.error('Error retrieving order:', error);
+        res.status(500).send('An error occurred while retrieving the order');
+    }
+});
+
+app.post('/update-order/:id', authenticate, async (req, res) => {
+    try {
+        const orderId = req.params.id;
+        const { customerName, products } = req.body;
+
+        // Find the order in the database
+        const order = await Order.findById(orderId);
+
+        if (!order) {
+            return res.status(404).send('Order not found');
+        }
+
+        // Update the order data
+        order.customerName = customerName;
+        order.products = products;
+
+        // Save the updated order
+        await order.save();
+
+        res.redirect('/manage-orders');
+    } catch (error) {
+        console.error('Error updating order:', error);
+        res.status(500).send('An error occurred while updating the order');
+    }
+});
+
+
+
 
 
 
