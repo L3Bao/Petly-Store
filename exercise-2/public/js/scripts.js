88f4ed83bdbe15690b965f43a585227ccee50cd6
Myiea <110724590+Myiea@users.noreply.github.com>
2023-05-16 00:01:54 +0700

    update vendor and customer page

--- exercise-2/public/js/scripts.js
+++ exercise-2/public/js/scripts.js
@@ -1,2 +1,126 @@
-// You can add your own js script here!
-console.log("Hello world!");
+'use strict';
+
+
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navToggler = document.querySelector("[data-nav-toggler]");
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggler.classList.toggle("active");
+}
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+
+
+/**
+ * active header when window scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const activeElemOnScroll = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", activeElemOnScroll);
+
+
+
+
+
+
+
+/*Myie*/
+/*auto calculate using price and quantity*/
+function calculate() {
+  var firstNumber = document.getElementById("price").innerHTML;
+  var secondNumber = document.getElementById("quantity").innerHTML;
+  var result = parseInt(firstNumber) + parseInt(secondNumber);
+  document.getElementById("result").innerHTML = result;
+}
+
+window.onload = calculate;
+
+/*active close button on mobile display*/
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar-items');
+
+if (bar){
+    bar.addEventListener('click', () =>{
+        nav.classList.add('active')
+    })
+}
+
+if (close){
+    close.addEventListener('click',() =>{
+        nav.classList.remove('active');
+    })
+}
+
+/*change price depends on the type of products*/
+function changePrice() {
+    var bookType = document.getElementById("book-type").value;
+    var bookPrice = document.getElementById("price");
+    if (bookType === "hardback") {
+      bookPrice.innerHTML = "$12.00";
+    } else {
+      bookPrice.innerHTML = "$5.00";
+    }
+  }
+  
+/*view small picture in main picture upon click*/
+var bigImg = document.getElementById("main-img");
+var smallImg = document.getElementsByClassName("smalImg")
+
+smallImg[0].onclick = function(){
+    bigImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function(){
+    bigImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function(){
+    bigImg.src = smallImg[2].src;
+}
+
+function rst(){
+    document.getElementById('form').reset();
+}
